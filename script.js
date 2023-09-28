a0e77f181a3e8cdf6f5de7b81bd41b6e5a6d7777
Bruno Campos <143615932+Cap-Bruno@users.noreply.github.com>
2023-09-28 12:16:46 -0300

    Todos arquivos

---
+++ script.js
@@ -0,0 +1,14 @@
+(function (){
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    console.log(names)
+
+    for (var name in names) {
+        var firstLetter = names[name].charAt(0);
+        if((firstLetter == 'j') || (firstLetter == 'J')){
+            byeSpeaker.speak(names[name]);
+        }
+        else {
+            helloSpeaker.speak(names[name]);
+        }
+    }
+})();
